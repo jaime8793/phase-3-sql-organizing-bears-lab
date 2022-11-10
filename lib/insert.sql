1cd94c7d32fefb01578ca6eaa588a60d217f1a9f
jaime8793 <109973167+jaime8793@users.noreply.github.com>
2022-11-10 03:49:45 +0300

    Completed Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr Chocolate', 102, 'M', 'brown', 'grumpy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 15, 'M', 'white', 'grumpy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 75, 'F', 'black', 'grumpy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergent Brown', 256, 'M', 'brown', 'grumpy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 255, 'F', 'white', 'grumpy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 45, 'M', 'brown', 'grumpy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Panda', 2, 'M', 'grey', 'grumpy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL , 5, 'F', 'brown', 'grumpy', 1);
